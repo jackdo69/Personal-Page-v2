b0f29bf9ffeabe18ea8f9f5d30f9ba228f2fbdb9
jackdo69 <jackie.duc.do@gmail.com>
2020-01-22 15:58:42 +1100

    hide preload after 2s

--- app.js
+++
@@ -1,9 +0,0 @@
-// const preloader = document.querySelector('#preloader path');
-// console.log(`The pattern is ${preloader.getTotalLength()}`);
-//Get the number input for preloader stroke
-
-
-
-
-
-// End of nav and active class section
---
+++ js/preload.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $('.preload').fadeOut(500, "linear")
+    }, 2000)
+});
